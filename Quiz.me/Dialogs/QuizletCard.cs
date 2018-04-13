83aaad05bda9301cd711f5cd6424996426424d5f
Erick Suarez <mr.ericksuarez@gmail.com>
2018-04-12 18:22:34 -0700

    Add Quizlet JSON parsing

---
+++ Quiz.me/Dialogs/QuizletCard.cs
@@ -0,0 +1,11 @@
+﻿namespace Quiz.me.Dialogs
+{
+    public class QuizletCard
+    {
+        public string id { get; set; }
+        public string term { get; set; }
+        public string definition { get; set; }
+        public string image { get; set; }
+        public string rank { get; set; }
+    }
+}
--- Quiz.me/Dialogs/RootDialog.cs
+++ Quiz.me/Dialogs/RootDialog.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
 
 namespace Quiz.me.Dialogs
 {
@@ -17,15 +22,79 @@ namespace Quiz.me.Dialogs
 
         private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
         {
-            var activity = await result as Activity;
+            var reply = context.MakeMessage();
 
-            // calculate something for us to return
-            int length = (activity.Text ?? string.Empty).Length;
+            // Quizlet API
+            var myUri = new Uri("https://api.quizlet.com/2.0/sets/415/terms?client_id=zeSpVr8wzT&whitespace=1");
+            var myWebRequest = WebRequest.Create(myUri);
+            var myHttpWebRequest = (HttpWebRequest)myWebRequest;
+            myHttpWebRequest.PreAuthenticate = true;
+            myHttpWebRequest.Headers.Add("Authorization", "Bearer brkshEQjyE");
+            myHttpWebRequest.Accept = "application/json";
+
+            var myWebResponse = myWebRequest.GetResponse();
+            var responseStream = myWebResponse.GetResponseStream();
+            if (responseStream == null) Console.WriteLine("RESPONSE STREAM IS NULL");
+
+            var myStreamReader = new StreamReader(responseStream, Encoding.Default);
+            var json = myStreamReader.ReadToEnd();
+            var data = JsonConvert.DeserializeObject<List<QuizletCard>>(json);
+            responseStream.Close();
+            myWebResponse.Close();
+
+
+            // Add an InputHint to let Cortana know to expect user input
+            reply.Text = data[0].id; ;
+            reply.Speak = "This is the text that will be spoken.";
+            reply.InputHint = InputHints.IgnoringInput;
 
             // return our reply to the user
-            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+            await context.PostAsync(reply);
 
             context.Wait(MessageReceivedAsync);
         }
     }
 }
+/*
+ * 
+ *             reply.AttachmentLayout = AttachmentLayoutTypes.Carousel;
+            reply.Attachments = new List<Attachment>();
+            var connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+
+            Dictionary<string, string> cardContentList = new Dictionary<string, string>();
+            cardContentList.Add("PigLatin", "https://loremflickr.com/320/240");
+            cardContentList.Add("Pork Shoulder", "https://loremflickr.com/320/240");
+            cardContentList.Add("Bacon", "https://loremflickr.com/320/240");
+
+            foreach (KeyValuePair<string, string> cardContent in cardContentList)
+            {
+                List<CardImage> cardImages = new List<CardImage>();
+                cardImages.Add(new CardImage(url: cardContent.Value));
+
+                List<CardAction> cardButtons = new List<CardAction>();
+
+                CardAction plButton = new CardAction()
+                {
+                    Value = $"https://en.wikipedia.org/wiki/{cardContent.Key}",
+                    Type = "openUrl",
+                    Title = "WikiPedia Page"
+                };
+
+                cardButtons.Add(plButton);
+
+                HeroCard plCard = new HeroCard()
+                {
+                    Title = $"I'm a hero card about {cardContent.Key}",
+                    Subtitle = $"{cardContent.Key} Wikipedia Page",
+                    Images = cardImages,
+                    Buttons = cardButtons
+                };
+
+                Attachment plAttachment = plCard.ToAttachment();
+                reply.Attachments.Add(plAttachment);
+            }
+
+            await connector.Conversations.SendToConversationAsync(reply);
+
+
+*/
